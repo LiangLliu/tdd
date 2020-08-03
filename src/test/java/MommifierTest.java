30019b2abd351a603712bd75ac103ca123abf464
Liangliang Liu <max1141847753@gmail.com>
2020-08-03 11:02:09 +0800

    return empty string when input empty string

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MommifierTest {
+
+    @Test
+    void should_return_empty_string_input_empty_string() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert("");
+        assertEquals("", result);
+    }
 }
